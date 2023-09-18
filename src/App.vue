5aa41da85489b0784f5fd72f1ce8c23ffab90975
RuslanGH2023 <144463626+RuslanGH2023@users.noreply.github.com>
2023-09-18 12:01:25 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>My Cool Vue App</h1>
+        <button>App Task</button>
+        </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<style>
+
+</style>
+
